a050a05915e6570d2fce199c94a302d2eda8879d
Marko Miseljic <marko.miseljic.14@gmail.com>
2021-12-10 15:08:02 +0000

    Restricted some command only for valid users, command for shoving count of members

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import random
 from replit import db
 from keep_alive import keep_alive
 
+gmessages = joined = 0
 
 client = discord.Client()
 
@@ -40,20 +41,41 @@ def delete_encouragement(index):
     del encouragements[index]
     db["encouragements"] = encouragements
 
+
+@client.event
+async def on_member_join(member):
+  for channel in member.guild.channels:
+    if str(channel) == "general":
+      await channel.send_message(f"""Welcome to the server {member.mention}""")
+
+
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
+  global gmessages
+  gmessages += 1
   if message.author == client.user:
     return
 
   msg = message.content
+
+  id = client.get_guild(int(os.getenv("ID")))
+  channels = ["general"]
+  valid_users = ["extreme1337#5244"]
+
+  if str(message.channel) in channels and str(message.author) in valid_users:
+        if msg.find("!hello") != -1:
+            await message.channel.send("Hi") 
+        elif msg == "!users":
+            await message.channel.send(f"""# of Members: {id.member_count}""")
   
   if msg.startswith("$inspire"):
     quote = get_quote()
     await message.channel.send(quote)
+
 
   if db["responding"]:
     options = starter_encouragements
